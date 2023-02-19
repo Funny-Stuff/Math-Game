256df847ddb39095b46be176d419b5ee7fc333bc
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-02-18 23:23:18 -0300

    Funny Instruction Removal

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,11 +1,4 @@
-﻿/*   1. You need to create a Math game containing the 4 basic operations
- *   2. The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100.
- *   3. Users should be presented with a menu to choose an operation
- *   4. You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
- *   5. You don't need to record results on a database. Once the program is closed the results will be deleted.
- */
-using System;
-namespace MathGame;
+﻿namespace MathGame;
 
 public static class Program {
     private static Random random = new();
